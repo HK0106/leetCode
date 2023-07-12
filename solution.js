2cb698b0745b38690b966f59c18db932bf8aaffa
MaiDX <cangao1504@gmail.com>
2023-07-12 23:16:22 +0700

    commit test 1

---
+++ solution.js
@@ -0,0 +1,39 @@
+//Link: https://leetcode.com/problems/find-eventual-safe-states/
+
+
+/**
+ * @param {number[][]} graph
+ * @return {number[]}
+ */
+const eventualSafeNodes = function(graph) {
+  const visit = new Array(graph.length).fill(0);
+  const res = [];
+  const dfs = (node) => {
+    if (visit[node] === 1 ) return false;
+    if (visit[node] === 2 ) return true;
+
+    visit[node] = 1;
+
+    for (let edge of graph[node]) {
+      if (dfs(edge)) {
+        return false;
+      }
+    }
+
+    visit[node] = 2;
+    return true;
+  }
+  for (let i = 0; i < graph.length; i++) {
+    if (dfs(i)) {
+      res.push(i);
+    }
+  }
+
+  return res;
+};
+
+
+console.log(eventualSafeNodes([[1,2],[2,3],[5],[0],[5],[],[]]))
+
+
+//[[1,3,4],[2,1],[],[0],[5],[]]
