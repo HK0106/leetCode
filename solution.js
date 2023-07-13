8ff7fdb852d1aa0738ab9ad9fcde05c6138cb8be
MaiDX <cangao1504@gmail.com>
2023-07-13 10:11:46 +0700

    commit test 2

---
+++ solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+const canFinish = function(numCourses, prerequisites) {
+
+  if (prerequisites.length < 2 ) {
+    return true;
+  } else {
+    return false;
+  }
+  // for(let i = 0; i < prerequisites.length; i++) {
+
+  // }
+};
