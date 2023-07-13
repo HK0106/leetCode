a0a1aeea431e20e6363650e0bbc9523a4788adda
MaiDX <cangao1504@gmail.com>
2023-07-13 14:14:57 +0700

    commit test 2

--- solution.js
+++ solution.js
@@ -4,13 +4,58 @@
  * @return {boolean}
  */
 const canFinish = function(numCourses, prerequisites) {
+  const graph = {};
+  
+  // nead to read before read key
+  for (const [a,b] of prerequisites) {
+    
+    if (!graph[a]) {
+      graph[a] = [];
+    }
+    
+    if (!graph[b]) {
+      graph[b] = [];
+    }
+    
+    graph[b].push(a)
+  }
+  
+  //Init DFS
+  let visiting = new Set();
+  let visited = new Set();
 
-  if (prerequisites.length < 2 ) {
-    return true;
-  } else {
+  for (const node in graph) {
+    if (hasCycle(node)) {
+      return false;
+    }
+  }
+
+
+  function hasCycle (node) {
+    if (visiting.has(node)) {
+      return true;
+    }
+    if (visited.has(node)) {
+      return false;
+    }
+    visiting.add(node);
+
+    for (let child of graph[node]) {
+      if (hasCycle(child)) {
+        return true;
+      }
+    }
+
+    visiting.delete(node);
+    visited.add(node);
     return false;
   }
-  // for(let i = 0; i < prerequisites.length; i++) {
-
-  // }
+  return true;
 };
+
+
+
+console.log(canFinish(5,[[1,4],[2,4],[3,1],[3,2]]), ': => Expect true');
+console.log(canFinish(5,[[1,4],[2,4],[4,2],[3,1],[3,2]]), ': => Expect false');
+console.log(canFinish(2,[[1,0],[0,1]]), ': => Expect false');
+console.log(canFinish(2,[[1,0]]), ': => Expect true');
