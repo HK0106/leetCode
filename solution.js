74e84a1af30dd350f85cc6d32395630b095a86fb
maidx <maidx.xm@gmail.com>
2023-10-03 14:57:47 +0700

    Update solution

---
+++ solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+const merge = function(nums1, m, nums2, n) {
+    for (let i = m, j = 0; j < n; i++, j++) {
+        nums1[i] = nums2[j];
+    }
+    nums1.sort((a, b) => a - b)
+};
+
+const merge1 = function(nums1, m, nums2, n) {
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+
+    while (j >= 0) {
+        if (i >= 0 && nums1 > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
+    }
+};
