442307ed596fda71e6b916fb97187acfe02c55ee
maidx <maidx.xm@gmail.com>
2023-10-16 14:18:23 +0700

    Update solution (re-learn)

--- solution.js
+++ solution.js
@@ -4,18 +4,30 @@
  * @return {boolean}
  */
 const isAnagram = function(s, t) {
-  const sortedS = s.split('').sort().join('');
-  const sortedT = t.split('').sort().join('');
   if (s.length !== t.length) {
     return false;
-  } else {
-    for (let i = 0; i < s.length; i++) {
-     if (sortedS[i] !== sortedT[i]) {
-       return  false
-     }
-    }
-    return true;
   }
+  const maps = {};
+  const mapt = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if(maps[s[i]]) {
+      maps[s[i]] += 1;
+    } else {
+      maps[s[i]] = 1;
+    }
+    if(mapt[t[i]]) {
+      mapt[t[i]] += 1;
+    } else {
+      mapt[t[i]] = 1;
+    }
+  }
+  for (const prop in maps) {
+    if (maps[prop] !== mapt[prop]) {
+      return false
+    }
+  }
+  return true;
 };
 
 console.log(isAnagram('anagram','nagaram'))
