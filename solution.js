3e400c457ced7bc0c3c230fbe846b17e6969a487
maidx <maidx.xm@gmail.com>
2023-10-19 06:17:51 +0700

    Update solution

--- solution.js
+++ solution.js
@@ -3,56 +3,23 @@
  * @return {number}
  */
 const trap = function(height) {
-    let sumResult = 0;
-    if (height.length === 0) {
-        return sumResult;
-    }
-    let leftMax = [];
-    let rightMax = [];
+    let L = 0, maxLeft = 0, maxRight = 0, sum = 0;
+    let R = height.length -1;
 
+    while (L < R) {
+        if (height[L] <= height[R]) {
 
-    let tempMax = -1;
-    for (let i = 0; i < height.length; i++) {
-        if (i === 0) {
-            leftMax.push(0);
+            if (height[L] > maxLeft) maxLeft = height[L]
+            else sum += maxLeft - height[L]
+            L++;
         } else {
-            if (height[i - 1] > tempMax) {
-                tempMax = height[i - 1];
-                leftMax.push(height[i - 1]);
-            } else {
-                leftMax.push(tempMax)
-            }
-        }
 
-
-    }
-    tempMax = -1;
-    for (let i = height.length - 1; i >= 0; i--) {
-        if (i === height.length -1) {
-            rightMax[height.length-1]=0;
-        } else {
-            if (height[i+1] > tempMax) {
-                tempMax = height[i+1];
-                rightMax[i] = height[i+1]
-            } else {
-                rightMax[i] = tempMax;
-            }
-        }
-
-    }
-    for (let i = 0; i < height.length; i++) {
-        let tempWater = 0;
-        if (leftMax[i] <= rightMax[i]) {
-            tempWater += leftMax[i] - height[i];
-        } else {
-            tempWater += rightMax[i] - height[i];
-        }
-        if (tempWater > 0) {
-            sumResult += tempWater;
+            if (height[R] > maxRight) maxRight = height[R]
+            else sum += maxRight -height[R]
+            R--;
         }
     }
-
-    return sumResult
+    return sum;
 };
 
 console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1]), `Expect [6]`)
