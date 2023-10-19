29b312340c6eb887a46757ea2b79640aa2851c87
maidx <maidx.xm@gmail.com>
2023-10-19 09:02:56 +0700

    Update solution

--- solution.js
+++ solution.js
@@ -7,13 +7,12 @@ const search = function(nums, target) {
     let low = 0, high = nums.length -1
     while (low < high) {
         let mid = low + Math.floor((high - low + 1) /2)
-        if (target < nums[high]) {
+        if (target < nums[mid]) {
             high = mid - 1;
         } else {
             low = mid;
         }
     }
-    console.log("2",nums[low])
     return nums[low] === target ? low : -1
 };
 
