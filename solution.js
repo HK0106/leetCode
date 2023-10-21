0ff33bb1fc7df2aae3bb0a0adb106347d3f743e7
maidx <maidx.xm@gmail.com>
2023-10-21 08:48:50 +0700

    Update solution

--- solution.js
+++ solution.js
@@ -1,32 +1,20 @@
 /**
- * @param {number} target
- * @param {number[]} position
- * @param {number[]} speed
+ * @param {number[]} heights
  * @return {number}
  */
-const carFleet = function(target, position, speed) {
-    const getTime = ([pos, sp]) => (target - pos) / sp
-    const stack = []
-
-    const times = position
-        .map((v, i) => [v, speed[i]])
-        .sort((a, b) => a[0] - b[0])
-        .map(getTime)
-
-    for (const time of times) {
-        while (stack.length && stack.at(-1) <= time) {
-            stack.pop()
+const largestRectangleArea = function(heights) {
+    let stack = [];
+    let result = 0;
+    for (let i = 0; i < heights.length; i++) {
+        let heightStart = i;
+        while (stack.length && stack[stack.length - 1][1] > heights[i]) {
+            let [pos, height] = stack.pop();
+            result = Math.max(result, (i - pos) * height);
+            heightStart = pos;
         }
-
-        stack.push(time)
+        stack.push([heightStart, heights[i]]);
     }
-
-    return stack.length
-
 };
 
-// console.log(carFleet(12,[10,8,0,5,3],[2,4,1,1,3]))
-// console.log(carFleet(10,[3],[3]))
-// console.log(carFleet(100,[0,2,4],[4,2,1]))
-// console.log(carFleet(10,[6,8],[3,2]))
-console.log(carFleet(10,[0,4,2],[2,1,3]))
+console.log(largestRectangleArea([2,1,5,6,2,3]), 'Expected: 10')
+// console.log(largestRectangleArea([2,4]), 'Expected: 4')
